cf33e593c102539abf3640dbe1be7f87d2747101
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2015-12-18 19:56:05 +0100

    Defined interfaces for Input and Output 'gates'

---
+++ Devices/IInput.py
@@ -0,0 +1,9 @@
+class IInput:
+    def get_name(self):
+        pass
+
+    def set_name(self, name):
+        pass
+
+    def get_value(self):
+        pass
---
+++ Devices/IOutput.py
@@ -0,0 +1,12 @@
+class IOutput:
+    def get_name(self):
+        pass
+
+    def set_name(self, name):
+        pass
+
+    def get_value(self):
+        pass
+
+    def set_value(self, value):
+        pass
