a8dcb97de4bb5d5bebf9d382606d92706a80234f
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2015-12-18 17:37:36 +0100

    Defined basic interfaces.

---
+++ Devices/IDevice.py
@@ -0,0 +1,27 @@
+
+
+class IDevice:
+
+    def get_name(self):
+        pass
+
+    def set_name(self, name):
+        pass
+
+    def get_description(self):
+        pass
+
+    def set_description(self, description):
+        pass
+
+    def get_status(self):
+        pass
+
+    def get_io_list(self):
+        pass
+
+    def get_output(self, name):
+        pass
+
+    def get_input(self, name):
+        pass
---
+++ Devices/IDimmerChannel.py
@@ -0,0 +1,27 @@
+
+
+class IDimmerChannel:
+
+    def set_name(self, name):
+        pass
+
+    def get_name(self):
+        pass
+
+    def set_description(self, description):
+        pass
+
+    def get_description(self):
+        pass
+
+    def set_intensity(self, intensity):
+        pass
+
+    def get_intensity(self):
+        pass
+
+    def set_dimming_type(self, dimming_type):
+        pass
+
+    def get_dimming_type(self):
+        pass
