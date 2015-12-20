76915175f6c5dac2dd74e09ddaffcbcbad188e97
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2015-12-20 15:28:43 +0100

    Extended definition of IInput interface. Also specify the type of input.

--- Devices/IInput.py
+++ Devices/IInput.py
@@ -7,3 +7,10 @@ class IInput:
 
     def get_value(self):
         pass
+
+    def get_type(self):
+        """
+        An input can have different types (analog, digital, counter, etc)
+        :return:
+        """
+        pass
