8722790b08c5b216393683339cc7e752d3a5ceb1
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2015-12-20 15:29:24 +0100

    Communicator interface definition for the Arduino.

---
+++ Hardware/IArduinoCommunicator.py
@@ -0,0 +1,10 @@
+class IArduinoCommunicator:
+
+    def send_command(self, address, sub_address, data):
+        pass
+
+    def request_data(self, address, sub_address):
+        pass
+
+    def add_observer(self, observer):
+        pass
