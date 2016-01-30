d49f5f3d721d39810c08c6a49023b0491c38d5c4
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2016-01-30 15:32:34 +0100

    Rewriting the Arduino class.

--- Devices/Arduino.py
+++ Devices/Arduino.py
@@ -1,103 +1,71 @@
 from Devices.ADevice import ADevice
-from Devices.ArduinoDigitalInput import ArduinoDigitalInput
-from Hardware.IArduinoCommunicator import IArduinoCommunicator
-from Devices.IInput import IInput
+from Communicators.IArduinoCommunicator import IArduinoCommunicator
 
 
 class Arduino(ADevice):
-    __input_list = {}
-    __output_list = {}
-    __communicator = None
-    __address = ''
+    """
+    The Arduino is one of the supported devices in the web application. In the domotica project there is always one
+    Arduino connected to the Raspberry-pi. It can be used to connect to I2C sensors, read analog or digital inputs,
+    dim lights or control digital outputs, depending on the connected hardware.
+    """
 
-    __used_ports = []
-    ANALOG_INPUT_MAPPING = {
-        'A0': 'PC0',
-        'A1': 'PC1',
-        'A2': 'PC2',
-        'A3': 'PC3',
-        'A4': 'PC4',
-        'A5': 'PC5',
-        'A6': 'PC6',
-        'A7': 'PC7'
-    }
-    DIGITAL_INPUT_MAPPING = {
-        'D0': 'PD0',
-        'D1': 'PD1',
-        'D2': 'PD2',
-        'D3': 'PD3',
-        'D4': 'PD4',
-        'D5': 'PD5',
-        'D6': 'PD6',
-        'D7': 'PD7',
-        'D8': 'PB0',
-        'D9': 'PB1',
-        'D10': 'PB2',
-        'D11': 'PB3',
-        'D12': 'PB4',
-        'D13': 'PB5',
-        'A0': 'PC0',
-        'A1': 'PC1',
-        'A2': 'PC2',
-        'A3': 'PC3',
-        'A4': 'PC4',
-        'A5': 'PC5',
-    }
+    def __init__(self, communicator, name, description=''):
+        """
+        In the constructor, we require access to a communication device. We also need to specify a name and description
+        for the Device.
 
-    DATA_MAPPING = {
-    }
-
-    def __init__(self, name, communicator, address, description=''):
+        :param communicator: A communication device. Used but not owned by the Arduino, as it could be shared among
+        other devices.
+        :param name: A name for the device being created. Used to identify devices.
+        :param description: A description for the device.
+        """
         super().__init__(name, description)
-        assert isinstance(communicator, IArduinoCommunicator)
+        assert (isinstance(communicator, IArduinoCommunicator))
         self.__communicator = communicator
-        self.__address = address
-        self.__status = 'Unknown'
 
     def get_io_list(self):
-        io_list = {'inputs': self.__input_list, 'outputs': self.__output_list}
-        return io_list
-
-    def get_status(self):
-        return self.__status
+        return super().get_io_list()
 
     def get_output(self, name):
-        assert name in self.__output_list
-        return self.__output_list[name]
+        super().get_output(name)
 
     def get_input(self, name):
-        assert name in self.__input_list
-        return self.__input_list[name]
+        super().get_input(name)
 
-    def add_input(self, type, port, name, options):
-        assert port not in self.__used_ports
+    def get_status(self):
+        return super().get_status()
 
-        if type == 'Analog':
-            self.__add_setup_analog_port()
-        elif type == 'Digital':
-            self.__setup_digital_input(port, name, options)
-        elif type == 'Counter':
-            self.__setup_counter_input(port, name, options)
-
-    def add_output(self, type, port, name, options):
-        # Todo: Implement function
+    def add_digital_output(self, name, description, pin):
+        """
+        Create a digital output, and also send the configuration command to the Arduino.
+        :param name: A string with the name for the digital output.
+        :param description: A string with the description for the digital output.
+        :param pin: A string specifying the pin of the Arduino that should be used.
+        :return: The created digital output, if creation was successful. It should be an implementation of the
+        IDigitalOutput interface.
+        """
+        # TODO: Implement function
         pass
 
-    def get_error_message(self):
-        return 'Not implemented'
+    def add_digital_input(self, name, description, pin):
+        """
+        Create a digital input, and also send the configuration command to the Arduino.
+        :param name: A string with the name for the digital input.
+        :param description: A string with the description for the digital input.
+        :param pin: A string specifying the pin of the Arduino that should be used.
+        :return: The created digital output. It should be an implementation of the IDigitalInput interface.
+        """
+        # TODO: Implement function
+        pass
 
-    def synchronize(self):
-        return 0
+    def __request_status(self):
+        # TODO: Implement the function
+        pass
 
-    def __add_setup_analog_port(self, port, name, options):
-        assert port in self.ANALOG_INPUT_MAPPING
-        # Todo: Implement the rest.
+    def __setup_digital_output(self):
+        # TODO: Implement the function
+        pass
 
-    def __setup_digital_input(self, port, name, options):
-        new_input = ArduinoDigitalInput(name, port, self.__communicator)
-        self.__communicator.send_command(self.__address, 0x00, 0x00)
-        self.__input_list[name] = new_input
-
-    def __setup_counter_input(self, port, name, options):
-        # Todo: Implement function
+    def __setup_digital_input(self):
+        # TODO: Implement the function
         pass
