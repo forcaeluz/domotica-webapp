e38532d2224e69d77a6fdae3d0e8f7872e4c1724
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2016-01-31 13:08:52 +0100

    Documented interface definition.

--- Communicators/IArduinoCommunicator.py
+++ Communicators/IArduinoCommunicator.py
@@ -1,10 +1,43 @@
 class IArduinoCommunicator:
+    """
+    The description of the interface to communicate with the Arduino. The interface describes how to control
+    and read IO of the Arduino. Another interface should be used to describe how to read debug messages
+    produced by the Arduino.
 
-    def send_command(self, address, sub_address, data):
+    The communication protocol is very similar to the CanOpen protocol. In the Arduino, "Objects" are the data
+    containers. You can write to an object, or read from an object. An object has an address and a sub-address,
+    and its functionality is described in the Arduino documentation.
+
+    """
+
+    def write_to_object(self, address, sub_address, data):
+        """
+        Used to send data to the Arduino. This function is blocking, and waits on the
+        reply of the Arduino.
+
+        :param address: Object's address.
+        :param sub_address: Object's sub-address.
+        :param data: Object's data.
+        """
         pass
 
-    def request_data(self, address, sub_address):
+    def request_object_data(self, address, sub_address):
+        """
+        Used to send data to the Arduino. This function is blocking, and waits on the
+        reply of the Arduino.
+        :param address: Object's address.
+        :param sub_address: Object's sub-address.
+        :return: The data in the specified object.
+        """
         pass
 
-    def add_observer(self, observer):
+    def add_observer(self, address, sub_address, observer):
+        """
+        Used to subscribe to automatic updates of the Arduino. Enabling automatic updates is done by
+        writing to objects, as described in the Arduino documentation. This function is only used to
+        assign a callback to the updates of a specific object.
+        :param address: Object's address.
+        :param sub_address: Object's sub-address.
+        :param observer:
+        """
         pass
